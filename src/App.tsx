c58a7f5965f18db0f50231dc43419bf01e13f57b
Juha Pekkarinen <juha.pekkarinen@codemate.com>
2021-04-16 13:46:00 +0300

    Add react-router and basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Todos from "./Todos";
+
 import "./App.scss";
-import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <TodoList />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/todo">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main>
+          <Switch>
+            <Route path="/todo">
+              <Todos />
+            </Route>
+
+            <Route>Pick some</Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
