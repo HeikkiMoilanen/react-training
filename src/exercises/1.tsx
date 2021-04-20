32084baf05d4bd749113dfb8fe174f95943e516a
Heikki Moilanen <heikki.moilanen@codemate.com>
2021-04-20 21:56:03 +0300

    add first exercise description

--- src/exercises/1.tsx
+++ src/exercises/1.tsx
@@ -3,8 +3,22 @@ import * as React from "react";
 import "components/HockeyCard/HockeyCard.scss";
 
 /**
+ * Please make a hockey card component that describes one player.
+ * You can make up the data.
+ *
+ * The card should show the player's full name and position.
+
+ * The card should also include a <table> with 3 <tr>: height, weight and birth date
+ * These rows should each have two <td>, e.g.
+ * <td>Height:</td>
+ * <td>6' 5"</td>
+ *
+ * the stylings have already been made if you use the following classes:
+ */
+
+/**
  * Classes:
- * - hockey-card
+ * - hockey-card__wrapper
  * - hockey-card__fullName
  * - hockey-card__position
  * - hockey-card__detailTable
